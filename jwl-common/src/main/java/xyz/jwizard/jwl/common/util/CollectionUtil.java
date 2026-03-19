945053f1cf6cfdcc284def2bb0d737588449ba93
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-03-19 23:33:49 +0100

    moves CollectionUtil to util package and adds private constructor

--- jwl-common/src/main/java/xyz/jwizard/jwl/common/util/CollectionUtil.java
+++ jwl-common/src/main/java/xyz/jwizard/jwl/common/util/CollectionUtil.java
@@ -1,9 +1,12 @@
-package xyz.jwizard.jwl.common;
+package xyz.jwizard.jwl.common.util;
 
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 public class CollectionUtil {
+    private CollectionUtil() {
+    }
+    
     @SafeVarargs
     public static <T> LinkedHashSet<T> linkedSetOf(T... elements) {
         return new LinkedHashSet<>(Arrays.asList(elements));
