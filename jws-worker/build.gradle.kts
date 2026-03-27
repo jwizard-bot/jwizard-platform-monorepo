bb30827588845835cf213d532cc5e81830307809
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-03-27 01:20:14 +0100

    refactor: jws-worker: remove jwl-persistence dependency

--- jws-worker/build.gradle.kts
+++ jws-worker/build.gradle.kts
@@ -2,7 +2,6 @@ dependencies {
     implementation(project(":jwl-common"))
     implementation(project(":jwl-contracts"))
     implementation(project(":jwl-http"))
-    implementation(project(":jwl-persistence"))
 
     testImplementation(testFixtures(project(":jwl-common")))
 }
