03550e37f09199dd524e41d82f74b0997026ae8b
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-04-03 02:18:50 +0200

    style: format ManagedDataSource record declaration

--- jwl-sql/src/main/java/xyz/jwizard/jwl/sql/pool/ManagedDataSource.java
+++ jwl-sql/src/main/java/xyz/jwizard/jwl/sql/pool/ManagedDataSource.java
@@ -17,8 +17,5 @@ package xyz.jwizard.jwl.sql.pool;
 
 import javax.sql.DataSource;
 
-public record ManagedDataSource(
-    DataSource dataSource,
-    Runnable closeAction
-) {
+public record ManagedDataSource(DataSource dataSource, Runnable closeAction) {
 }
