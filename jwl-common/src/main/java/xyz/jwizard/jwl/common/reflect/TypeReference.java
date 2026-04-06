202be0e16ca66717e1a80b22cf3d95fb46c8253f
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-04-06 19:21:25 +0200

    chore: suppress unused warning for TypeReference

--- jwl-common/src/main/java/xyz/jwizard/jwl/common/reflect/TypeReference.java
+++ jwl-common/src/main/java/xyz/jwizard/jwl/common/reflect/TypeReference.java
@@ -18,6 +18,7 @@ package xyz.jwizard.jwl.common.reflect;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
+@SuppressWarnings("unused")
 public abstract class TypeReference<T> {
     private final Type type;
 
