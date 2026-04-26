89033ed96a7327a9fd9371c54245a4f33b1b2de6
Miłosz Gilga <gilgamilosz451@gmail.com>
2026-04-26 02:15:30 +0200

    feat(ingestor): add websocket server lifecycle

--- jws-ingestor/build.gradle.kts
+++ jws-ingestor/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation(project(":jwl-codec"))
     implementation(project(":jwl-common"))
     implementation(project(":jwl-graph"))
-    implementation(project(":jwl-http"))
+    implementation(project(":jwl-websocket"))
 
     testImplementation(testFixtures(project(":jwl-common")))
 }
